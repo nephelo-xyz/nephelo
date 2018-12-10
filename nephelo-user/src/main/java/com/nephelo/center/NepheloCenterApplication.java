5aaed2c5ec823c2b6ccdec21b80ad30aa1771a97
nephelo <yanlin0802@qq.com>
2018-12-10 18:04:16 +0800

    -

---
+++ nephelo-user/src/main/java/com/nephelo/center/NepheloCenterApplication.java
@@ -0,0 +1,12 @@
+package com.nephelo.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepheloCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepheloCenterApplication.class, args);
+	}
+}

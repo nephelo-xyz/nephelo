f0c74e450fa7a5b5fe7fc15fc81cc6f928d8c5eb
nephelo <yanlin0802@qq.com>
2018-12-20 18:30:29 +0800

    1

--- nephelo-consumer/src/main/java/com/nephelo/consumer/NepheloConsumerApplication.java
+++ nephelo-consumer/src/main/java/com/nephelo/consumer/NepheloConsumerApplication.java
@@ -2,19 +2,19 @@ package com.nephelo.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
-@EnableCaching //开启redis缓存
-@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
 @EnableFeignClients
 @EnableZuulProxy
+@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
 public class NepheloConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NepheloConsumerApplication.class, args);

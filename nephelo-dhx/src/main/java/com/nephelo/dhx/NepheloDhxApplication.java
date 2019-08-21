623430ccbe870f671de87d4e5a7667246d8071f7
nephelo <yanlin0802@126.com>
2019-08-21 14:18:52 +0800

    默认更改列表

---
+++ nephelo-dhx/src/main/java/com/nephelo/dhx/NepheloDhxApplication.java
@@ -0,0 +1,13 @@
+package com.nephelo.dhx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepheloDhxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NepheloDhxApplication.class, args);
+    }
+
+}
